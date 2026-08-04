4a129aa21bca37bef23cd8b7bf82dfd92446a050
Dinil Kojitha <dinilkojitha@gmail.com>
2026-08-04 15:22:08 +0530

    Create project

---
+++ src/test/java/com/example/stockmanagementsystembackend/StockManagementSystemBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.stockmanagementsystembackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StockManagementSystemBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
