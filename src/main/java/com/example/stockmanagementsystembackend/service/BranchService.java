cad7c0a69b7014bcf6c37435bb4b80121824949b
Kojitha P D <dinilkojitha@gmail.com>
2026-08-26 22:12:54 +0530

    add user request and branch request

--- src/main/java/com/example/stockmanagementsystembackend/service/BranchService.java
+++ src/main/java/com/example/stockmanagementsystembackend/service/BranchService.java
@@ -1,10 +1,21 @@
 package com.example.stockmanagementsystembackend.service;
 
+import com.example.stockmanagementsystembackend.repository.BranchRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.PathVariable;
 
 @Service
 public class BranchService {
 
+    @Autowired
+    private BranchRepository branchRepository;
+
+//    public void deleteBranch(int id){
+//        branchRepository.deleteById(id);
+//        return ResponseEntity.ok();
+//    }
 
 
 
