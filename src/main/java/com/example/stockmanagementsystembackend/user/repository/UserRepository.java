37442c447409c337043a1889b89d47f246ecff9d
Kojitha P D <dinilkojitha@gmail.com>
2026-08-27 00:56:37 +0530

    add method to count users by department

--- src/main/java/com/example/stockmanagementsystembackend/user/repository/UserRepository.java
+++ src/main/java/com/example/stockmanagementsystembackend/user/repository/UserRepository.java
@@ -1,10 +1,11 @@
 package com.example.stockmanagementsystembackend.user.repository;
 
 import com.example.stockmanagementsystembackend.user.entity.User;
+import com.example.stockmanagementsystembackend.domain.organization.entity.Department;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, Integer> {
-
+	long countByDepartmentDepartment(Department department);
 }
