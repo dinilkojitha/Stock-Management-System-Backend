bd8c397ec5b10a92e8e831c79ae7922d3f70d796
ShamikhHameed <shamikhhameed@gmail.com>
2026-09-11 20:28:54 +0530

    Added 3 new classes, 1 new interface and introduced comments to each of the java files

--- src/main/java/com/example/stockmanagementsystembackend/domain/distribution/controller/InternalRequestController.java
+++ src/main/java/com/example/stockmanagementsystembackend/domain/distribution/controller/InternalRequestController.java
@@ -3,14 +3,45 @@
  */
 package com.example.stockmanagementsystembackend.domain.distribution.controller;
 
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.example.stockmanagementsystembackend.domain.distribution.entity.InternalRequest;
+import com.example.stockmanagementsystembackend.domain.distribution.service.InternalRequestService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @CrossOrigin
 @RestController
 @RequestMapping(value = "/api/internal-requests")
 public class InternalRequestController {
 
+    private final InternalRequestService internalRequestService;
+
+    public InternalRequestController(InternalRequestService internalRequestService){
+        this.internalRequestService=internalRequestService;
+    }
+
+    @GetMapping
+    public ResponseEntity<?> getALLRequest(){
+        return ResponseEntity.ok(internalRequestService.getAllRequest());
+
+    }
+
+    @PostMapping
+    public ResponseEntity<?> createRequest(@RequestBody InternalRequest request){
+        return ResponseEntity.ok(internalRequestService.createRequest(request));
+
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<?> updateRequest(
+            @PathVariable Integer id,
+            @RequestBody InternalRequest request) {
+        return ResponseEntity.ok(internalRequestService.updateRequest(id, request));
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<?> deleteRequest(@PathVariable Integer id) {
+        internalRequestService.deleteRequest(id);
+        return ResponseEntity.ok().build();
+    }
 
 }
--- src/main/java/com/example/stockmanagementsystembackend/domain/distribution/service/InternalRequestService.java
+++ src/main/java/com/example/stockmanagementsystembackend/domain/distribution/service/InternalRequestService.java
@@ -5,8 +5,43 @@ package com.example.stockmanagementsystembackend.domain.distribution.service;
 
 import com.example.stockmanagementsystembackend.domain.distribution.repository.InternalRequestRepository;
 import org.springframework.stereotype.Service;
+import com.example.stockmanagementsystembackend.domain.distribution.entity.InternalRequest;
+import java.util.List;
 
 @Service
 public class InternalRequestService {
+
+    private final InternalRequestRepository internalRequestRepository;
+
+    public InternalRequestService(InternalRequestRepository internalRequestRepository) {
+        this.internalRequestRepository=internalRequestRepository;
+    }
+
+    public List<InternalRequest> getAllRequest(){
+        return internalRequestRepository.findAll();
+    }
+
+    public InternalRequest createRequest(InternalRequest request){
+        return internalRequestRepository.save(request);
+    }
+
+    public InternalRequest updateRequest(Integer id, InternalRequest request){
+        InternalRequest existingRequest=
+                internalRequestRepository.findById(id)
+                        .orElseThrow(()-> new RuntimeException("Request not found"));
+
+        existingRequest.setDepartmentDepartment(request.getDepartmentDepartment());
+        existingRequest.setUserUserid(request.getUserUserid());
+        existingRequest.setRequestTime(request.getRequestTime());
+        existingRequest.setStatus(request.getStatus());
+
+        return internalRequestRepository.save(existingRequest);
+
+    }
+
+    public void deleteRequest(Integer id){
+        internalRequestRepository.deleteById(id);
+    }
+
 
 }
