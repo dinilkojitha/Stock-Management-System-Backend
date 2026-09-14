896a8b24b165533963a27aff9f09f7caae3fdae1
Sandani-Sathsarani <sandanisathsarani2005@gmail.com>
2026-09-14 12:10:18 +0530

    add-new-Transaction

--- src/main/java/com/example/stockmanagementsystembackend/domain/audit/controller/TransactionController.java
+++ src/main/java/com/example/stockmanagementsystembackend/domain/audit/controller/TransactionController.java
@@ -1,12 +1,21 @@
 package com.example.stockmanagementsystembackend.domain.audit.controller;
 
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.example.stockmanagementsystembackend.domain.audit.entity.Transaction;
+import com.example.stockmanagementsystembackend.domain.audit.service.TransactionService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
 
 @CrossOrigin
 @RestController
 @RequestMapping(value = "/api/transactions")
 public class TransactionController {
 
+    @Autowired
+    TransactionService transactionService;
+
+    @PostMapping("add-new")
+    public Transaction add (@PathVariable Transaction trans){
+        return transactionService.add(trans);
+    }
 }
--- src/main/java/com/example/stockmanagementsystembackend/domain/audit/service/TransactionService.java
+++ src/main/java/com/example/stockmanagementsystembackend/domain/audit/service/TransactionService.java
@@ -1,9 +1,23 @@
 package com.example.stockmanagementsystembackend.domain.audit.service;
 
+import com.example.stockmanagementsystembackend.domain.audit.entity.Transaction;
 import com.example.stockmanagementsystembackend.domain.audit.repository.TransactionRepository;
+import com.example.stockmanagementsystembackend.domain.audit.repository.TransactionRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.time.Instant;
+import java.util.List;
+
 
 @Service
 public class TransactionService {
 
+    @Autowired
+    TransactionRepository transactionRepository;
+
+    public Transaction add (Transaction trans){
+        return transactionRepository.save(trans);
+    }
 }
+
