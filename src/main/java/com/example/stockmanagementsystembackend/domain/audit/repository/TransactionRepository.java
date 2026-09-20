4aaae7d8e68c5b5748f14c0d8340e6bc3759a8c4
Sandani-Sathsarani <sandanisathsarani2005@gmail.com>
2026-09-20 18:20:49 +0530

    modify_repository

--- src/main/java/com/example/stockmanagementsystembackend/domain/audit/repository/TransactionRepository.java
+++ src/main/java/com/example/stockmanagementsystembackend/domain/audit/repository/TransactionRepository.java
@@ -4,7 +4,16 @@ import com.example.stockmanagementsystembackend.domain.audit.entity.Transaction;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface TransactionRepository extends JpaRepository<Transaction, Integer> {
 
+    List<Transaction> findByUserUseridId(Integer userId);
+
+    List<Transaction> findByItemId(Integer itemId);
+
+    List<Transaction> findByTransactionTypeIgnoreCase(String transactionType);
 }
+
+
