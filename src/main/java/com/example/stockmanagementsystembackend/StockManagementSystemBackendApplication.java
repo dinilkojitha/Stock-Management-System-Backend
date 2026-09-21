286250e4a54449562a1a403c6c8e056a9bb049ce
Minuka Dinsitha <minuka.dinsitha2005@gmail.com>
2026-09-21 14:49:45 +0530

    Backend Updated

---
+++ src/main/java/com/example/stockmanagementsystembackend/StockManagementSystemBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.stockmanagementsystembackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StockManagementSystemBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StockManagementSystemBackendApplication.class, args);
+    }
+
+}
