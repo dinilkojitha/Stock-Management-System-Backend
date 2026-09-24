57ca28dd2c41b3b22475119799c5795443e45afc
Sandani-Sathsarani <sandanisathsarani2005@gmail.com>
2026-09-24 18:24:42 +0530

    role-controller-and-role-service

--- src/main/java/com/example/stockmanagementsystembackend/user/controller/RoleController.java
+++ src/main/java/com/example/stockmanagementsystembackend/user/controller/RoleController.java
@@ -1,12 +1,33 @@
 package com.example.stockmanagementsystembackend.user.controller;
 
+import com.example.stockmanagementsystembackend.user.entity.Role;
+import com.example.stockmanagementsystembackend.user.service.RoleService;
+import jakarta.validation.Valid;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.net.URI;
+import java.util.List;
 
 @CrossOrigin
 @RestController
 @RequestMapping(value = "/api/roles")
 public class RoleController {
+
+    private final RoleService roleService;
+
+    public RoleController(RoleService roleService) {
+        this.roleService = roleService;
+    }
+
+
 
 }
--- src/main/java/com/example/stockmanagementsystembackend/user/service/RoleService.java
+++ src/main/java/com/example/stockmanagementsystembackend/user/service/RoleService.java
@@ -1,9 +1,20 @@
 package com.example.stockmanagementsystembackend.user.service;
 
 import com.example.stockmanagementsystembackend.user.repository.RoleRepository;
+import com.example.stockmanagementsystembackend.user.entity.Role;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.server.ResponseStatusException;
+
+import java.util.List;
 
 @Service
 public class RoleService {
+    private final RoleRepository roleRepository;
 
+    public RoleService(RoleRepository roleRepository) {
+        this.roleRepository = roleRepository;
+    }
 }
